9205aa944a366682b84eac3ef088f31bc96e04f8
dimfreelancer <dimfreelancerdn@gmail.com>
2020-10-17 21:10:21 +0300

    Add files via upload
    Загрузил домашку к Уроку 1 на курсе JS 15.0

---
+++ lesson01/main.js
@@ -0,0 +1,19 @@
+'use strict';
+
+console.log('Это первый Урок 1 Домашка');
+
+/* 
+let money;
+let income;
+let addExpenses;
+let deposit;
+let mission;
+let period;
+ */
+
+let money, income, addExpenses, deposit, mission, period;
+
+
+alert('Hello, this Lesson 1');
+console.log('Привет это сообщение простое в консоль');
+
