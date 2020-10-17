6c74c284059fa4b928fd310649556314b103176a
dimfreelancer <dimfreelancerdn@gmail.com>
2020-10-17 21:24:28 +0300

    загрузил файлы на домашку Урок 2

---
+++ lesson02/main.js
@@ -0,0 +1,16 @@
+'use strict';
+
+console.log('hallo this is main task обязательное задание');
+
+
+
+const money = 57000; //
+
+let income;
+const deposite = false;
+const addExpenses = '';
+
+
+let bungetDay;
+
+console.log(typeof income);
